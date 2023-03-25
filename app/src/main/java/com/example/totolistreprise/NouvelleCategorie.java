e7658d1681017da452c91c62f72bacaa458dfdd1
Jean Jonathan <jeanjonathank2@gmail.com>
2023-03-25 12:34:03 +0100

    Creation de l interface pour le bouton nouvelle Categorie

---
+++ app/src/main/java/com/example/totolistreprise/NouvelleCategorie.java
@@ -0,0 +1,14 @@
+package com.example.totolistreprise;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NouvelleCategorie extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nouvelle_categorie);
+    }
+}
